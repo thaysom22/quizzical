bc1fc54b5f32ce9e662afcace1f4de15bc7ab057
thaysom22 <thaysom22@gmail.com>
2021-04-09 08:55:34 +0000

    feat: partly implement login functionality

--- app.py
+++ app.py
@@ -26,31 +26,86 @@ def default():
     """
     docstring here
     """
-    loggedIn = 'user' in session  # check in cookies if user is logged in
-    if loggedIn == False:
-        return redirect(url_for("landing_page"))
-    else:
-        # get data from db
-        # pass username to discover_page view
-        return redirect(url_for("discover_page"))
+    loggedIn = 'user' in session  # check in session dict if user is logged in
+    if not loggedIn:
+        return redirect(url_for("landing"))
+   
+    # get data from db
+    # pass username to discover_page view
+    return redirect(url_for("discover"))
 
 @app.route("/landing")
-def landing_page():
+def landing():
     """
     docstring here
     """
     loggedIn = 'user' in session
-    return render_template("pages/landing.html",
-        loggedIn=loggedIn)
+    if loggedIn:
+        return redirect(url_for("discover"))
+
+    return render_template("pages/landing.html")
 
 @app.route("/discover")
-def discover_page():
+def discover():
     """
     docstring here
     """
     loggedIn = 'user' in session
-    return render_template("pages/discover.html",
-        loggedIn=loggedIn)
+    if not loggedIn:
+        return redirect(url_for("landing"))
+
+    return render_template("pages/discover.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """
+    username is not case-sensitive
+    password is case-sensitive
+    docstring here
+    """
+    loggedIn = 'user' in session
+    if loggedIn:
+        # get user_id from session dict and pass to redirect function
+        return redirect(url_for("profile"))
+    
+    if request.method == "GET":
+        return render_template("pages/login.html")
+
+    username_form = request.form.get("username").lower()  # get form data 
+    existing_user = mongo.db.users.find_one(
+        {"username": username_form})  # get user document from db (returns None if not found in db)
+    
+    if not existing_user:
+        flash("Incorrect Username and/or Password")  # notify user of failure
+        return redirect(url_for("login"))
+
+    password_form = request.form.get("password")  # get form data
+    correct_password = check_password_hash(existing_user["password"], password_form)
+
+    if not correct_password:
+        flash("Incorrect Username and/or Password")  # notify user of failure
+        return redirect(url_for("login")) 
+
+    session["user"] = existing_user["username"]  # create session dict 
+    flash(f"Welcome to Quizzical") #  flash message to newly logged in user on discover page
+    return redirect(url_for("discover"))
+
+
+    
+    # redirect to discover if successful
+
+    
+
+# receives user_id as param or gets it from session dict
+@app.route("/profile")
+def profile():
+    """
+    docstring here
+    """
+    loggedIn = 'user' in session
+    if not loggedIn:
+        return redirect(url_for("login"))
+
 
 
 if __name__ == "__main__":

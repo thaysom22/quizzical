b5f62398bb7232f3bc7ffb377f1333d7194c8ee8
thaysom22 <thaysom22@gmail.com>
2021-04-09 07:38:37 +0000

    feat: finish landing page: complete 'steps to get started' and final cta sections

--- app.py
+++ app.py
@@ -57,5 +57,5 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
-        debug=True  # update to False prior to cloud deployment
+        debug=os.environ.get("DEBUG") == "True"
     )
